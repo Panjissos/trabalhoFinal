01b40e710b3bb18036e0ae00b7cfaa1bbae67c47
Vitor Pantoja <vitortravassos3@gmail.com>
2022-07-10 21:18:09 -0300

    funcionando a criação do arquivo csv

--- lib.h
+++ lib.h
@@ -3,192 +3,218 @@
 #include <math.h>
 #include <dirent.h>
 #include <string.h>
-#include "pgm.h"
-
-
 #define TAMCHAR 256
-#define MAXFILESNAMES 1000
-#define MAXCHARNAME 100
+#define MAXF 1000
+#define MAXN 100
+#define T 8
 
-void filtrolbp(struct pgm *img, struct pgm *fil);
-void histogram(unsigned char *m, int l, int c, unsigned char *hist);
-void readPGMImage(struct pgm *pio, char *filename);
-void writePGMImage(struct pgm *pio, char *filename);
-void viewPGMImage(struct pgm *pio);
-void gravarEmCSV(const unsigned char *const , char *const);
+struct pgm{
+	int tipo;
+	int c;
+	int r;
+	int mv;
+	unsigned char *pData;
+};
+//criação das váriaveis que serão utilizadas na main
+struct pgm img;
+struct pgm *filterImg;
+unsigned char *hist;
+char nomeArquivo[MAXF][MAXN];
+DIR *dir;
+struct dirent *pImgArq;
+int iArquivo = 0;
+char identificador[TAMCHAR];
 
-// void readPGMImage(struct pgm *pio, char *filename){
 
-// 	FILE *fp;
-// 	char ch;
-// 	// printf("%s", filename);
+void filtrolbp(struct pgm *, struct pgm *);
+void histograma(unsigned char *, int , int , unsigned char *);
+void readPGMImage(struct pgm *, char *);
+void writePGMImage(struct pgm *, char *);
+void viewPGMImage(struct pgm *);
+void histogramaCSV(const unsigned char *const , char *const, char *);
 
-// 	if (!(fp = fopen(filename,"r"))){
-// 		perror("Erro.");
-// 		exit(1);
-// 	}
+void readPGMImage(struct pgm *pio, char *filename)
+{
 
-// 	if ( (ch = getc(fp))!='P'){
-// 		puts("A imagem fornecida não está no formato pgm");
-// 		exit(2);
-// 	}
-	
-// 	pio->tipo = getc(fp)-48;
-	
-// 	fseek(fp,1, SEEK_CUR);
+    FILE *fp;
+    char ch;
 
-// 	// pra vê se tem comentarios
-// 	while((ch=getc(fp))=='#'){
-// 		while( (ch=getc(fp))!='\n');
-// 	}
+    if (!(fp = fopen(filename, "r")))
+    {
+        perror("Erro.");
+        exit(1);
+    }
 
-// 	fseek(fp,-1, SEEK_CUR);
+    if ((ch = getc(fp)) != 'P')
+    {
+        puts("A imagem fornecida não está no formato pgm");
+        exit(2);
+    }
 
-// 	fscanf(fp, "%d %d",&pio->c,&pio->r);
-// 	if (ferror(fp)){ 
-// 		perror(NULL);
-// 		exit(3);
-// 	}	
-// 	fscanf(fp, "%d",&pio->mv);
-// 	fseek(fp,1, SEEK_CUR);
+    pio->tipo = getc(fp) - 48;
 
-// 	// dados da imagem
-// 	pio->pData = (unsigned char*) malloc(pio->r * pio->c * sizeof(unsigned char));
+    fseek(fp, 1, SEEK_CUR);
 
-// 	switch(pio->tipo){
-// 		case 2:
-// 			puts("Lendo imagem PGM (dados em texto)");
-// 			for (int k=0; k < (pio->r * pio->c); k++){
-// 				fscanf(fp, "%hhu", pio->pData+k);
-// 			}
-// 		break;	
-// 		case 5:
-// 			puts("Lendo imagem PGM (dados em binário)");
-// 			fread(pio->pData,sizeof(unsigned char),pio->r * pio->c, fp);
-// 		break;
-// 		default:
-// 			puts("Não está implementado");
-// 	}
-	
-// 	fclose(fp);
+    // pra vê se tem comentarios
+    while ((ch = getc(fp)) == '#')
+    {
+        while ((ch = getc(fp)) != '\n')
+            ;
+    }
 
-// }
+    fseek(fp, -1, SEEK_CUR);
 
-// void writePGMImage(struct pgm *pio, char *filename){
-// 	FILE *fp;
-// 	char ch;
+    fscanf(fp, "%d %d", &pio->c, &pio->r);
+    if (ferror(fp))
+    {
+        perror(NULL);
+        exit(3);
+    }
+    fscanf(fp, "%d", &pio->mv);
+    fseek(fp, 1, SEEK_CUR);
 
-// 	if (!(fp = fopen(filename,"wb"))){
-// 		perror("Erro.");
-// 		exit(1);
-// 	}
+    // dados da imagem
+    pio->pData = (unsigned char *)malloc(pio->r * pio->c * sizeof(unsigned char));
 
-// 	fprintf(fp, "%s\n","P5");
-// 	fprintf(fp, "%d %d\n",pio->c, pio->r);
-// 	fprintf(fp, "%d\n", 255);
+    switch (pio->tipo)
+    {
+    case 2:
+        puts("Lendo imagem PGM (dados em texto)");
+        for (int k = 0; k < (pio->r * pio->c); k++)
+        {
+            fscanf(fp, "%hhu", pio->pData + k);
+        }
+        break;
+    case 5:
+        puts("Lendo imagem PGM (dados em binário)");
+        fread(pio->pData, sizeof(unsigned char), pio->r * pio->c, fp);
+        break;
+    default:
+        puts("Não está implementado");
+    }
 
-// 	fwrite(pio->pData, sizeof(unsigned char),pio->c * pio->r, fp);
+    fclose(fp);
+}
 
-// 	fclose(fp);
+void writePGMImage(struct pgm *pio, char *filename)
+{
+    FILE *fp;
+    char ch;
 
-// }
+    if (!(fp = fopen(filename, "wb")))
+    {
+        perror("Erro.");
+        exit(1);
+    }
 
-// void viewPGMImage(struct pgm *pio){
-// 	printf("Tipo: %d\n",pio->tipo);
-// 	printf("Dimensões: [%d %d]\n",pio->c, pio->r);
-// 	printf("Max: %d\n",pio->mv);
+    fprintf(fp, "%s\n", "P5");
+    fprintf(fp, "%d %d\n", pio->c, pio->r);
+    fprintf(fp, "%d\n", 255);
 
-// 	for (int k=0; k < (pio->r * pio->c); k++){
-// 		if (!( k % pio->c)) printf("\n");
-// 		printf("%d ",*(pio->pData+k));
-// 	}	
-// 	printf("\n");
-// }
+    fwrite(pio->pData, sizeof(unsigned char), pio->c * pio->r, fp);
 
-// void gravarEmCSV(const unsigned char * hist, char *const fileName){
-//     FILE *arquivoCVS;
+    fclose(fp);
+}
 
-//     // adcionar um arquivo no fim de curso
-//     arquivoCVS = fopen("miauuuuuu.csv", "ab");
+void viewPGMImage(struct pgm *pio)
+{
+    printf("Tipo: %d\n", pio->tipo);
+    printf("Dimensões: [%d %d]\n", pio->c, pio->r);
+    printf("Max: %d\n", pio->mv);
+
+    for (int k = 0; k < (pio->r * pio->c); k++)
+    {
+        if (!(k % pio->c))
+            printf("\n");
+        printf("%d ", *(pio->pData + k));
+    }
+    printf("\n");
+}
+
+void histogramaCSV(const unsigned char *hist, char *const nomeArquivo, char * identificador)
+{
+    FILE *arquivoCVS;
+
+    // adcionar um arquivo no fim de curso
+    arquivoCVS = fopen("outputHistogram.csv", "ab");
+
+    for (int i = 0; i < 256; i++)
+    {
+        fprintf(arquivoCVS, "%d,", *(hist + i));
+    }
     
-//     for(int i = 0; i < 256; i++){
-//         // printf("%d, ", *(hist+i));
-//         fprintf(arquivoCVS, "%d,", *(hist+i));
-//     }
+    fprintf(arquivoCVS, "%c\n", identificador);
 
-//     fprintf(arquivoCVS, "%c\n", fileName[1]);
+    fclose(arquivoCVS);
+}
 
-//     fclose(arquivoCVS);
+void filtrolbp(struct pgm *img, struct pgm *imgFiltro)
+{
+    // Filtro LBP. Janela de 8 bits. Método Comparativo
+    // Prenchimento da estrutura
+    imgFiltro->tipo = 5;
+    imgFiltro->r = img->r;
+    imgFiltro->c = img->c;
+    imgFiltro->mv = 255;
+    imgFiltro->pData = (unsigned char *)calloc(imgFiltro->r * imgFiltro->c, sizeof(unsigned char));
 
-// }
+    // Execução do filtro LBP
+    int l = imgFiltro->r, c = imgFiltro->c;  // linhas e colunas
+    unsigned char soma, bjanela; // soma dos bits das janelas e bit da janela
+    int j, k, sent, pos, aux;    // j = posição linha, k = posição coluna, sent = sentido
+                                 // de rotação, pos = posição do bit na janela
 
-// void filtrolbp(struct pgm *img, struct pgm *fil) {
-//   // Filtro LBP. Janela de 8 bits. Método Comparativo
-//   // Prenchimento da estrutura
-//   fil->tipo = 5;
-//   fil->r = img->r;
-//   fil->c = img->c;
-//   fil->mv = 255;
-//   fil->pData = (unsigned char *)calloc(fil->r * fil->c, sizeof(unsigned char));
+    for (int i = 0; i < l * c; i++)
+    { // i = posição ponteiro inicial
+        soma = 0, j = -1, k = -1, sent = 1, pos = 0;//mantido
+               
+        for (int p = 0; p < T; p++)        
+        {
+            bjanela = 0; // Valor da janela de filtro, 0 para fora da matriz
 
-//   /*  
-//   000
-//   000
-//   000
-//   */
+            if ((i < c && j == -1) || (!(i % c) && k == -1) || (i > (l * c) - c && j == 1) || (!((i + 1) % c) && k == 1));
+            
+            else bjanela = *(img->pData + i + k + j * c);//atualiza a janela fora da matriz, com a 0, pois foi alocada antes com zeros
+            
+            if (bjanela >= *(img->pData + i)) soma += pow(2, pos);
+            
+            pos++;
 
-//   // Execução do filtro LBP
-//   int l = fil->r, c = fil->c;  // linhas e colunas
-//   unsigned char soma, bjanela; // soma dos bits das janelas e bit da janela
-//   int j, k, sent, pos; // j = posição linha, k = posição coluna, sent = sentido
-//                        // de rotação, pos = posição do bit na janela
+            switch (sent)
+            { // J e K desloca a janela, sent Muda o Sentido da janela
+            case 1:
+                k++;
+                if (k == 1)
+                    sent++;
+                break;
+            case 2:
+                j++;
+                if (j == 1)
+                    sent++;
+                break;
+            case 3:
+                k--;
+                if (k == -1)
+                    sent++;
+                break;
+            case 4:
+                j--;
+                if (j == -1)
+                    sent++;
+                break;
+            }
+        }
+       
+        *(imgFiltro->pData + i) = soma; // Recebe a soma dos bits no ponteiro de saída
+    }
+    
+}
 
-//   for (int i = 0; i < l * c; i++) { // i = posição ponteiro inicial
-//     soma = 0, j = -1, k = -1, sent = 1, pos = 0;
-//     do {
-//       bjanela = 0; // Valor da janela de filtro, 0 para fora da matriz
+void histograma(unsigned char *m, int l, int c, unsigned char *hist)
+{
+    for (int i = 0; i < l * c; i++)
+    {
+        *(hist + *(m + i)) += 1;
+    }  
 
-//       if (i < c && j == -1) {                 // superior
-//       } else if (!(i % c) && k == -1) {       // esquerdo
-//       } else if (i > (l * c) - c && j == 1) { // inferior
-//       } else if (!((i + 1) % c) && k == 1) {  // direito
-//       } else {
-//         bjanela = *(img->pData + i + k + j * c);
-//       }
-//       if (bjanela  >= *(img->pData + i)) { // metodo comparativo
-//         soma += pow(2, pos);
-//       }
-//       pos++;
-
-//       switch (sent) { // J e K desloca a janela, sent Muda o Sentido da janela
-//       case 1:
-//         k++;
-//         if (k == 1)
-//           sent++;
-//         break;
-//       case 2:
-//         j++;
-//         if (j == 1)
-//           sent++;
-//         break;
-//       case 3:
-//         k--;
-//         if (k == -1)
-//           sent++;
-//         break;
-//       case 4:
-//         j--;
-//         if (j == -1)
-//           sent++;
-//         break;
-//       }
-//     } while (sent <= 4);
-//     *(fil->pData + i) = soma; // Recebe a soma dos bits no ponteiro de saída
-//   }
-// }
-
-// void histogram(unsigned char *m, int l, int c, unsigned char *hist) {
-//   for (int i = 0; i < l * c; i++) {
-//     *(hist + *(m + i)) += 1 ;
-//   }
-// }
+}
--- main.c
+++ main.c
@@ -1,272 +1,31 @@
-//#include "lib.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <dirent.h>
-#include <string.h>
-#include "pgm.h"
-
-
-#define TAMCHAR 256
-#define MAXFILESNAMES 1000
-#define MAXCHARNAME 100
+#include "lib.h"
 
 int main(int argc, char *argv[])
 {
+    filterImg = malloc(sizeof(struct pgm));  
 
-    struct pgm img;
-    struct pgm *filterImg;
-    unsigned char *hist;
-    char fileNames[MAXFILESNAMES][MAXCHARNAME];
+    dir = opendir("./imgs/");  
+   
+    while ((pImgArq = readdir(dir)) != NULL){
 
-    filterImg = malloc(sizeof(struct pgm));
+        strcpy(nomeArquivo[iArquivo], "./imgs/");
+        identificador[iArquivo] = pImgArq->d_name[0];
+        strcat(nomeArquivo[iArquivo], pImgArq->d_name);
+        iArquivo++;
 
-    DIR *dir;
-    struct dirent *lsdir;
-
-    dir = opendir("./imgs/oncotex_pgm/");
-
-    int count = 0;
-    /* print all the files and directories within directory */
-    while ((lsdir = readdir(dir)) != NULL)
-    {
-        strcpy(fileNames[count], "./imgs/oncotex_pgm/");
-        strcat(fileNames[count], lsdir->d_name);
-        count++;
     }
 
-    for (int i = 2; i < count; i++)
-    {
-        // printf("%s \n", teste[i]);
-        readPGMImage(&img, fileNames[i]);
+    for (int i = 2; i < iArquivo; i++){
+
+        readPGMImage(&img, nomeArquivo[i]);
         filtrolbp(&img, filterImg);
         hist = malloc(TAMCHAR * sizeof(unsigned int));
-        histogram(filterImg->pData, filterImg->r, filterImg->c, hist);
-        gravarEmCSV(hist, fileNames[i]);
+        histograma(filterImg->pData, filterImg->r, filterImg->c, hist);
+        histogramaCSV(hist, nomeArquivo[i], identificador[i]);
 
-        // teste
-        FILE *arquivoCVS;
-        arquivoCVS = fopen("miauuuuuu.csv", "ab");
-
-        for (int i = 0; i < 256; i++)
-        {
-            // printf("%d, ", *(hist+i));
-            fprintf(arquivoCVS, "%d,", *(hist + i));
-        }
-
-        fprintf(arquivoCVS, "%c\n", fileNames[1]);
-
-        fclose(arquivoCVS);
-        //teste
     }
 
     closedir(dir);
 
     return 0;
-}
-
-void readPGMImage(struct pgm *pio, char *filename)
-{
-
-    FILE *fp;
-    char ch;
-    // printf("%s", filename);
-
-    if (!(fp = fopen(filename, "r")))
-    {
-        perror("Erro.");
-        exit(1);
-    }
-
-    if ((ch = getc(fp)) != 'P')
-    {
-        puts("A imagem fornecida não está no formato pgm");
-        exit(2);
-    }
-
-    pio->tipo = getc(fp) - 48;
-
-    fseek(fp, 1, SEEK_CUR);
-
-    // pra vê se tem comentarios
-    while ((ch = getc(fp)) == '#')
-    {
-        while ((ch = getc(fp)) != '\n')
-            ;
-    }
-
-    fseek(fp, -1, SEEK_CUR);
-
-    fscanf(fp, "%d %d", &pio->c, &pio->r);
-    if (ferror(fp))
-    {
-        perror(NULL);
-        exit(3);
-    }
-    fscanf(fp, "%d", &pio->mv);
-    fseek(fp, 1, SEEK_CUR);
-
-    // dados da imagem
-    pio->pData = (unsigned char *)malloc(pio->r * pio->c * sizeof(unsigned char));
-
-    switch (pio->tipo)
-    {
-    case 2:
-        puts("Lendo imagem PGM (dados em texto)");
-        for (int k = 0; k < (pio->r * pio->c); k++)
-        {
-            fscanf(fp, "%hhu", pio->pData + k);
-        }
-        break;
-    case 5:
-        puts("Lendo imagem PGM (dados em binário)");
-        fread(pio->pData, sizeof(unsigned char), pio->r * pio->c, fp);
-        break;
-    default:
-        puts("Não está implementado");
-    }
-
-    fclose(fp);
-}
-
-void writePGMImage(struct pgm *pio, char *filename)
-{
-    FILE *fp;
-    char ch;
-
-    if (!(fp = fopen(filename, "wb")))
-    {
-        perror("Erro.");
-        exit(1);
-    }
-
-    fprintf(fp, "%s\n", "P5");
-    fprintf(fp, "%d %d\n", pio->c, pio->r);
-    fprintf(fp, "%d\n", 255);
-
-    fwrite(pio->pData, sizeof(unsigned char), pio->c * pio->r, fp);
-
-    fclose(fp);
-}
-
-void viewPGMImage(struct pgm *pio)
-{
-    printf("Tipo: %d\n", pio->tipo);
-    printf("Dimensões: [%d %d]\n", pio->c, pio->r);
-    printf("Max: %d\n", pio->mv);
-
-    for (int k = 0; k < (pio->r * pio->c); k++)
-    {
-        if (!(k % pio->c))
-            printf("\n");
-        printf("%d ", *(pio->pData + k));
-    }
-    printf("\n");
-}
-
-void gravarEmCSV(const unsigned char *hist, char *const fileName)
-{
-    FILE *arquivoCVS;
-
-    // adcionar um arquivo no fim de curso
-    arquivoCVS = fopen("miauuuuuu.csv", "ab");
-
-    for (int i = 0; i < 256; i++)
-    {
-        // printf("%d, ", *(hist+i));
-        fprintf(arquivoCVS, "%d,", *(hist + i));
-        puts("miau");
-        printf("%c", *(hist + i));
-    }
-
-    fprintf(arquivoCVS, "%c\n", fileName[1]);
-
-    fclose(arquivoCVS);
-}
-
-void filtrolbp(struct pgm *img, struct pgm *fil)
-{
-    // Filtro LBP. Janela de 8 bits. Método Comparativo
-    // Prenchimento da estrutura
-    fil->tipo = 5;
-    fil->r = img->r;
-    fil->c = img->c;
-    fil->mv = 255;
-    fil->pData = (unsigned char *)calloc(fil->r * fil->c, sizeof(unsigned char));
-
-    /*
-    000
-    000
-    000
-    */
-
-    // Execução do filtro LBP
-    int l = fil->r, c = fil->c;  // linhas e colunas
-    unsigned char soma, bjanela; // soma dos bits das janelas e bit da janela
-    int j, k, sent, pos;         // j = posição linha, k = posição coluna, sent = sentido
-                                 // de rotação, pos = posição do bit na janela
-
-    for (int i = 0; i < l * c; i++)
-    { // i = posição ponteiro inicial
-        soma = 0, j = -1, k = -1, sent = 1, pos = 0;
-        do
-        {
-            bjanela = 0; // Valor da janela de filtro, 0 para fora da matriz
-
-            if (i < c && j == -1)
-            { // superior
-            }
-            else if (!(i % c) && k == -1)
-            { // esquerdo
-            }
-            else if (i > (l * c) - c && j == 1)
-            { // inferior
-            }
-            else if (!((i + 1) % c) && k == 1)
-            { // direito
-            }
-            else
-            {
-                bjanela = *(img->pData + i + k + j * c);
-            }
-            if (bjanela >= *(img->pData + i))
-            { // metodo comparativo
-                soma += pow(2, pos);
-            }
-            pos++;
-
-            switch (sent)
-            { // J e K desloca a janela, sent Muda o Sentido da janela
-            case 1:
-                k++;
-                if (k == 1)
-                    sent++;
-                break;
-            case 2:
-                j++;
-                if (j == 1)
-                    sent++;
-                break;
-            case 3:
-                k--;
-                if (k == -1)
-                    sent++;
-                break;
-            case 4:
-                j--;
-                if (j == -1)
-                    sent++;
-                break;
-            }
-        } while (sent <= 4);
-        *(fil->pData + i) = soma; // Recebe a soma dos bits no ponteiro de saída
-    }
-}
-
-void histogram(unsigned char *m, int l, int c, unsigned char *hist)
-{
-    for (int i = 0; i < l * c; i++)
-    {
-        *(hist + *(m + i)) += 1;
-    }
 }
